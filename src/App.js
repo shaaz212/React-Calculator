1012e4a9d0b3c812461cb50d6b46cd72bf675595
shaaz212 <shahabasha212@gmail.com>
2023-11-02 20:40:07 +0530

    react-calculator

--- src/App.js
+++ src/App.js
@@ -35,38 +35,38 @@ function App() {
 
   return (
     <div className='h-screen w-screen bg-sky-800 flex justify-center'>
-        <div className='w-3/5 md:w-2/5 h-3/5 flex justify-center'>
+        <div className='w-screen md:w-2/5 h-3/5 flex justify-center'>
           <div className='flex-col w-full'>
             <h1 className='text-center font-extrabold mt-5 mb-4' style={{color:'#3cdfff'}}>REACT CALCULATOR</h1>
-            <input type="text" value={value} className='bg-gray-800 w-11/12 md:w-full h-16 rounded-lg text-white text-right text-2xl md:text-3xl px-3 outline-none' placeholder='0' readOnly/>
+            <input type="text" value={value} className='bg-gray-800 w-full h-16 rounded-lg text-white text-right text-2xl md:text-3xl px-3 outline-none' placeholder='0' readOnly/>
             <div className='mt-2 w-full h-14 md:h-16'>
-            <Button className='w-1/5 md:w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>allclr()}>AC</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>removeLast()}>del</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("%")}>%</Button>
-            <Button className='w-1/4 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("÷")}>÷</Button>
+            <Button className='w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>allclr()}>AC</Button>
+            <Button className='w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>removeLast()}>del</Button>
+            <Button className='w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("%")}>%</Button>
+            <Button className='w-1/6 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("÷")}>÷</Button>
             </div>
           <div className='mt-2 w-full h-14 md:h-16'>
-            <Button className='w-1/5 md:w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("7")}>7</Button>
-            <Button className='w-1/5 md:w-1/4 h h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("8")}>8</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("9")}>9</Button>
-            <Button className='w-1/4 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("×")}>×</Button>
+            <Button className='w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("7")}>7</Button>
+            <Button className='w-1/4 h h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("8")}>8</Button>
+            <Button className='w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("9")}>9</Button>
+            <Button className='w-1/6 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("×")}>×</Button>
           </div>
           <div className='mt-2 w-full h-14 md:h-16'>
-            <Button className='w-1/5 md:w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("4")}>4</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("5")}>5</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("6")}>6</Button>
-            <Button className='w-1/4 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("-")}>-</Button>
+            <Button className='w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("4")}>4</Button>
+            <Button className='w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("5")}>5</Button>
+            <Button className='w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("6")}>6</Button>
+            <Button className='w-1/6 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("-")}>-</Button>
           </div>
           <div className='mt-2 w-full h-14 md:h-16'>
-            <Button className='w-1/5 md:w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("1")}>1</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("2")}>2</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("3")}>3</Button>
-            <Button className='w-1/4 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("+")}>+</Button>
+            <Button className='w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("1")}>1</Button>
+            <Button className='w-1/4 h-full me-2' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("2")}>2</Button>
+            <Button className='w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("3")}>3</Button>
+            <Button className='w-1/6 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>display("+")}>+</Button>
           </div>
           <div className='mt-2 w-full h-14 md:h-16'>
-            <Button className='w-5/12 md:w-1/2 h-full me-3' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("0")}>0</Button>
-            <Button className='w-1/5 md:w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display(".")}>.</Button>
-            <Button className='w-1/4 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>evalcal()}>=</Button>
+            <Button className='w-1/2 h-full me-3' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display("0")}>0</Button>
+            <Button className='w-1/4 h-full me-2.5' style={{backgroundColor:"#00C0F0",fontWeight:"bold"}} onClick={()=>display(".")}>.</Button>
+            <Button className='w-1/6 md:w-1/5 h-full' style={{backgroundColor:"#005063",fontWeight:"bold"}} onClick={()=>evalcal()}>=</Button>
           </div>
           </div>
         </div>
